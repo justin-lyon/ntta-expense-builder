525a2345951cc7c1fc28bdb85b154f9f120bbe40
Justin Lyon <jml6487@gmail.com>
2019-03-28 10:54:42 -0500

    replace upload button with drop zone

--- components/fileUpload.vue
+++ components/fileUpload.vue
@@ -1,14 +1,22 @@
 <template>
   <div>
-    <v-btn large color="orange"
-      @click="clickUpload" >
-      <v-icon medium >cloud_upload</v-icon>
-    </v-btn>
+    <div :id="dropzoneId"
+      class="dropzone"
+      @dragenter="dragOn"
+      @dragover="dragOn"
+      @dragleave="dragOff"
+      @dragend="dragOff"
+      @drop="drop"
+      @click="clickUpload">
+
+      <v-icon class="icon-size"
+        color="orange" >cloud_upload</v-icon>
+    </div>
     <input :id="inputId"
       name="fileUploader"
       type="file"
       @change="fileChanged"
-      accept=".csv" >
+      accept=".do" >
 
     <v-list two-line>
       <template v-for="(file, i) in files">
@@ -28,26 +36,37 @@ export default {
   data () {
     return {
       inputId: null,
+      dropzoneId: null,
       files: null
     }
   },
 
   mounted () {
     this.inputId = this._uid + '_fileInput'
+    this.dropzoneId = this._uid + '_dropzone'
   },
 
   methods: {
+    getFileInput () {
+      return document.getElementById(this.inputId)
+    },
+
     clickUpload () {
-      const el = document.getElementById(this.inputId)
+      const el = this.getFileInput()
       el.click()
     },
 
     fileChanged (event) {
-      const files = document.getElementById(this.inputId).files
+      const files = this.getFileInput().files
       this.files = files
       console.log('file', this.files)
 
       this.readFile(this.files[0])
+    },
+
+    setFiles (files) {
+      const fileInput = this.getFileInput()
+      this.files = files
     },
 
     clearFiles () {
@@ -65,6 +84,61 @@ export default {
       }
 
       reader.readAsText(file)
+    },
+
+    getDropzone () {
+      return document.getElementById(this.dropzoneId)
+    },
+
+    dragOn (event) {
+      event.preventDefault()
+      const dropzone = this.getDropzone()
+      dropzone.classList.add('active')
+    },
+
+    dragOff (event) {
+      event.preventDefault()
+      const dropzone = this.getDropzone()
+      dropzone.classList.remove('active')
+    },
+
+    drop (event) {
+      event.preventDefault()
+      const dropzone = this.getDropzone()
+      dropzone.classList.remove('active')
+
+      if (event.dataTransfer.items) {
+        const items = event.dataTransfer.items
+        console.log('items', items)
+
+        if (items.length > 1) {
+          throw new Error('Too many files.')
+        }
+
+        const files = Array.from(items).map(item => {
+          if (item.kind !== 'file') {
+            throw new Error('This is not a file.', item)
+          }
+
+          const file = item.getAsFile()
+          console.log('we got a file', file.name)
+          return file
+        });
+
+        this.setFiles(files)
+
+      } else {
+        const files = event.dataTransfer.files
+        console.log('files', files)
+
+        if (files.length > 1) {
+          throw new Error('Too many Files.')
+        }
+
+        Array.from(files).forEach(file => {
+          console.log('file', file.name)
+        })
+      }
     }
   },
 
@@ -75,6 +149,27 @@ export default {
 </script>
 
 <style scoped>
+div.dropzone {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  border: 1px dashed #ff9800;
+  border-radius: 1rem;
+  min-height: 10rem;
+  width: 80%;
+  margin: auto;
+}
+
+div.dropzone:hover, div.dropzone.active {
+  /* box-shadow: 10px 10px 5px #ff9800; */
+  border: 2px solid #ff9800;
+}
+
+.icon-size {
+  font-size: 8rem;
+}
+
 input[type="file"] {
   display: none;
 }
--- pages/index.vue
+++ pages/index.vue
@@ -5,13 +5,22 @@
         <v-card>
           <v-card-title>NTTA</v-card-title>
           <v-card-text>
-            Login to NTTA and export your transactions for excel.
+            <p>Login to NTTA and export your transactions for excel.</p>
+
+            <ol>
+              <li>Login to NTTA</li>
+              <li>Navigate to Account History</li>
+              <li>Filter by Date Range</li>
+              <li>Click "Download in Excel"</li>
+            </ol>
+
+            <p>NTTA Excel export is of file type ".do".</p>
           </v-card-text>
         </v-card>
       </v-flex>
       <v-flex xs12 >
         <v-card>
-          <v-card-title>content</v-card-title>
+          <v-card-title>Upload</v-card-title>
           <div class="text-xs-center">
             <app-file-upload></app-file-upload>
           </div>
