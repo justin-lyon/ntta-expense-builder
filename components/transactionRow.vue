4e461846c9e825455d5550b7c6142d78f18b7736
Justin Lyon <jml6487@gmail.com>
2019-04-15 12:44:29 -0500

    replace table with v-data-table

--- components/transactionRow.vue
+++ components/transactionRow.vue
@@ -1,8 +1,16 @@
 <template>
-  <tr>
+  <tr :active="item.selected" @click="item.selected = !item.selected">
+    <td>
+      <v-checkbox
+        :input-value="item.selected"
+        primary
+        hide-details
+      ></v-checkbox>
+    </td>
     <td>{{ postedDate }}</td>
     <td>{{ item.tagId }}</td>
     <td>{{ item.transactionType }}</td>
+    <td>{{ item.amount }}</td>
   </tr>
 </template>
 
--- components/transactionTable.vue
+++ components/transactionTable.vue
@@ -1,18 +1,40 @@
 <template>
-  <table>
-    <thead>
-      <tr>
-        <th>1</th>
-        <td>2</td>
-        <td>3</td>
-      </tr>
-    </thead>
-    <tbody>
-      <template v-for="(t, i) in transactions" >
-        <app-transaction-row :item="t" :key="t.postedDate + t.transactionType + i"></app-transaction-row>
+  <div>
+    <v-data-table
+      :headers="headers"
+      :items="transactions"
+      class="elevation-1">
+
+      <template v-slot:headers="props">
+        <tr>
+          <th>
+            <v-checkbox
+              :input-value="props.all"
+              :indeterminate="props.indeterminate"
+              primary
+              hide-details
+              @click.stop="toggleAll"
+            ></v-checkbox>
+          </th>
+          <th
+            v-for="header in props.headers"
+            :key="header.text"
+            :class="['column sortable', pagination.descending ? 'desc' : 'asc', header.value === pagination.sortBy ? 'active' : '']"
+            @click="changeSort(header.value)"
+          >
+            <v-icon small>arrow_upward</v-icon>
+            {{ header.text }}
+          </th>
+        </tr>
       </template>
-    </tbody>
-  </table>
+      <template v-slot:items="props">
+        <app-transaction-row
+          :item="props.item"
+          :key="props.item.exitDateTime + props.item.transactionType">
+        </app-transaction-row>
+      </template>
+    </v-data-table>
+  </div>
 </template>
 
 <script>
@@ -20,10 +42,42 @@ import { mapGetters } from 'vuex'
 import AppTransactionRow from './transactionRow'
 
 export default {
+  name: 'transactionTable',
+
+  data () {
+    return {
+      selected: [],
+      pagination: {
+        sortBy: 'postedDate'
+      },
+      headers: [
+        { text: 'Posted Date', sortable: true, value: 'postedDate' },
+        { text: 'Tag ID', value: 'tagId', sortable: true },
+        { text: 'Type', value: 'transactionType', sortable: true },
+        { text: 'Amount', value: 'amount' }
+      ]
+    }
+  },
+
   computed: {
     ...mapGetters('transactions', {
       transactions: 'getTransactions'
     })
+  },
+
+  methods: {
+    toggleAll () {
+      if (this.selected.length) this.selected = []
+      else this.selected = this.transactions.slice()
+    },
+    changeSort (column) {
+      if (this.pagination.sortBy === column) {
+        this.pagination.descending = !this.pagination.descending
+      } else {
+        this.pagination.sortBy = column
+        this.pagination.descending = false
+      }
+    }
   },
 
   components: {
